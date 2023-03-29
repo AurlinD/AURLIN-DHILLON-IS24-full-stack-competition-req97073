e043772bea5811e8fc771968a642f894570f7f55
Aurlin Dhillon <aurlindhillon@gmail.com>
2023-03-28 22:16:55 -0700

    set API endpoints for GET, POST, DELETE, GET by id

---
+++ backend/app.js
@@ -0,0 +1,96 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const { faker } = require("@faker-js/faker");
+const router = express.Router();
+const cors = require("cors");
+
+// initialize mock data array
+const db = Array(40)
+  .fill()
+  .map(() => ({
+    productId: faker.datatype.uuid(),
+    productName: faker.word.noun(),
+    productOwnerName: faker.name.firstName(),
+    Developers: Array(Math.floor(Math.random() * 5) + 1)
+      .fill()
+      .map(() => faker.name.firstName()),
+    scrumMasterName: faker.name.firstName(),
+    startDate: faker.datatype.datetime(),
+    methodology: faker.word.adjective(),
+  }))
+  .reduce((acc, curr) => {
+    acc[curr.productId] = curr;
+    return acc;
+  }, {});
+
+// do this better
+app.use(cors());
+
+app.use(express.json());
+
+// healthcheck
+router.get("/healthcheck", (req, res) => {
+  const data = {
+    uptime: process.uptime(),
+    message: "Ok",
+    date: new Date(),
+  };
+  res.status(200).send(data);
+});
+
+// get all
+router.get("/", (req, res) => {
+  res.json(Object.values(db));
+});
+
+// get specific
+router.get("/:productId", (req, res) => {
+  let product = db[req.params.productId];
+
+  if (product === undefined) {
+    res.status(404).json({ message: "Cannot find product" });
+    return;
+  }
+  res.json(product);
+});
+
+// post request
+router.post("/", (req, res) => {
+  const product = {
+    productId: req.body.productId,
+    productName: req.body.productName,
+    productOwnerName: req.body.productOwnerName,
+    Developers: ["Clifford"],
+    scrumMasterName: req.body.scrumMasterName,
+    startDate: req.body.startDate,
+    methodology: req.body.methodology,
+  };
+
+  if (db[product.productId]) {
+    res.status(400).json({
+      message:
+        "There already exists a productId in the db with the productId you have chosen",
+    });
+    return;
+  }
+
+  db[product.productId] = product;
+  res.status(201).json(product);
+});
+
+// delete by ID
+router.delete("/:productId", (req, res) => {
+  console.log(req.params.productId, db[req.params.productId]);
+  if (db[req.params.productId] === undefined) {
+    res.status(404).json({ message: "productId does not exist in the db" });
+    return;
+  }
+  delete db[req.params.productId];
+  res.json({ message: "Successfully deleted product" });
+});
+
+app.use("/api", router);
+
+app.listen(3008, () => console.log("Server started"));
