c3eb5e7a479a28ef8fdebb147f09d7c3a7f6cb65
Aurlin Dhillon <aurlindhillon@gmail.com>
2023-03-30 15:59:23 -0700

    moved button position for consistency

--- frontend/src/pages/EditProduct.jsx
+++ frontend/src/pages/EditProduct.jsx
@@ -6,10 +6,10 @@ const EditProduct = () => {
 
   return (
     <section>
-      <h1>Edit Product</h1>
       <button onClick={() => navigate("/", { replace: true })}>
         Go Back To List View
       </button>
+      <h1>Edit Product</h1>
       <EditProductForm />
     </section>
   );
