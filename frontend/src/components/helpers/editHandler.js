177eb4a029b9ca5ec68da2289ea7a5f442670e8b
Aurlin Dhillon <aurlindhillon@gmail.com>
2023-03-30 15:17:41 -0700

    remove console.log

--- frontend/src/components/helpers/editHandler.js
+++ frontend/src/components/helpers/editHandler.js
@@ -1,6 +1,5 @@
 const editHandler = (product, navigate) => {
   navigate(`/edit-product/${product.productId}`, { replace: true });
-  console.log("edit", product);
 };
 
 export default editHandler;
