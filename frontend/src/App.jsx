e910ea934b28d008e71e73248c94c0476de47547
Aurlin Dhillon <aurlindhillon@gmail.com>
2023-03-31 13:57:56 -0700

    css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,12 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import AllProducts from "./pages/AllProducts";
 import NewProduct from "./pages/NewProduct";
 import EditProduct from "./pages/EditProduct";
+import "./App.css";
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter className="App">
         <Routes>
           <Route exact path="/" element={<AllProducts />} />
           <Route exact path="/new-product" element={<NewProduct />} />
