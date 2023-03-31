ca9cb22375e6a74049a2140ca8415e02283d9b8b
Aurlin Dhillon <aurlindhillon@gmail.com>
2023-03-30 17:57:09 -0700

    documentation

--- frontend/src/components/api/deleteData.js
+++ frontend/src/components/api/deleteData.js
@@ -11,7 +11,7 @@ const deleteData = (product, input, setProducts) => {
   axios
     .delete(`http://localhost:3008/api/delete/${product.productId}`)
     .then(() => {
-      // after successfull deletion, call GET API to update the list.
+      // after successful deletion, call GET API to update the list.
       getData(input, setProducts);
     })
     .catch((err) => {
